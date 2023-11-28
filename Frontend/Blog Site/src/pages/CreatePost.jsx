1554939d44682d1ccc6118e52f8d9cb4914fe926
Ankit Das <www.das5471@gmail.com>
2023-11-29 02:46:02 +0530

    created  create post page

--- Frontend/Blog Site/src/pages/CreatePost.jsx
+++ Frontend/Blog Site/src/pages/CreatePost.jsx
@@ -1,8 +1,73 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
 
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+    toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{'list':'ordered'},{'list':'bullet'},{'indent':'-1'},{'indent':'+1'}],
+        ['link', 'image'],
+        ['clean']
+    ]
+}
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+]
 
 const CreatePost = () => {
+    const [title,setTitle] = useState('');
+    const [summary,setSummary] = useState('');
+    const [content,setContent] = useState('');
+    const [file,setFile] = useState('');
+    function createNewPost(e){
+        e.preventDefault();
+        //makinf data set for the from to send 
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', file);
+
+        //sending the data to the server 
+        fetch('http://localhost:4000/post',{
+            method: 'POST',
+            body: data, 
+        });
+    }
+    
   return (
-    <div>CreatePost</div>
+    <form className="createPost" onSubmit={createNewPost}>
+        <input 
+            type="text" 
+            placeholder={'Title'} 
+            value={title} 
+            onChange={e => setTitle(e.target.value)}
+        />
+        <input 
+            type="text" 
+            placeholder={'Summary'} 
+            value={summary} 
+            onChange={e => setSummary(e.target.value)}
+        />
+        <input
+            type="file"
+            onChange={e => setFile(e.target.files[0])}
+        />
+        <ReactQuill 
+            theme="snow" 
+            value={content} 
+            onChange={e => setContent(e)}
+            modules={modules} 
+            formats={formats} />
+        <button>Publish</button>
+        
+
+    </form>
   )
 }
 
